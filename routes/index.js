b04e4eed74197cf4e1a64bbebda2b2e972ad2e0e
Christopher Hauser <chauser6@gmail.com>
2021-11-10 13:40:45 -0500

    phases 1 and 2 done

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.json({
+        message: "test root index"
+    });
+    res.send("Welcome to the express-sequelize-starter!");
+  });
+
+
+module.exports = router;
